31265ad6aefc29ffc9f6a79743c4c7206516974e
Artem <artemmakarov76@yandex.ru>
2023-04-01 18:36:44 +0300

    done

--- src/UI/select/Select.jsx
+++ src/UI/select/Select.jsx
@@ -10,7 +10,10 @@ export default function Select({options, defaultOption, value, onChangeCallback}
     >
       <option disabled className={style.option}>{defaultOption}</option>
       {
-        options.map((option) => <option key={crypto.randomUUID()} value={option.value}>{option.name}</option>)
+        options.map((option) => 
+          <option key={crypto.randomUUID()} value={option.value}>
+            {option.name}
+          </option>)
       }
     </select>
   )
